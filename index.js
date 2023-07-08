d4c049972f9770911d34697f4c7a0c5d6707173c
Nessie <63102193+Nessie-Dev@users.noreply.github.com>
2023-07-08 15:08:46 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = require('node-fetch');
+const axios = require('axios');
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
@@ -17,8 +17,8 @@ app.get('/', (req, res) => {
 
 app.get('/ip', async (req, res) => {
   try {
-    const response = await fetch('https://api.ipify.org?format=json');
-    const data = await response.json();
+    const response = await axios.get('https://api.ipify.org?format=json');
+    const data = response.data;
     const serverIP = data.ip;
     res.json({ serverIP });
   } catch (error) {
@@ -34,8 +34,8 @@ app.get('/api/image/imgtotext', async (req, res) => {
   }
 
   try {
-    const response = await fetch(imageUrl);
-    const buffer = Buffer.from(await response.arrayBuffer());
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' });
+    const buffer = Buffer.from(response.data);
 
     const imagePath = path.join(__dirname, 'temp', 'generateImage.png');
     await writeFileAsync(imagePath, buffer);
@@ -44,14 +44,12 @@ app.get('/api/image/imgtotext', async (req, res) => {
     form.append('apikey', 'iz2P3HPSiKV5WQau01jCzCwODVzxRjB1');
     form.append('image', fs.createReadStream(imagePath), 'generateImage.png');
 
-    const apiResponse = await fetch('https://goatbot.tk/api/image/imgtotext', {
-      method: 'POST',
-      body: form,
+    const apiResponse = await axios.post('https://goatbot.tk/api/image/imgtotext', form, {
       headers: {
+        ...form.getHeaders(),
         'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
         'accept-language': 'en-US,en;q=0.9',
         'cache-control': 'max-age=0',
-        'content-type': `multipart/form-data; boundary=${form._boundary}`,
         'if-none-match': 'W/"83-xHLLcJsT2lN0RiI9GnijI4GswMQ"',
         'sec-ch-ua': '"Not.A/Brand";v="8", "Chromium";v="114", "Google Chrome";v="114"',
         'sec-ch-ua-mobile': '?0',
@@ -67,9 +65,9 @@ app.get('/api/image/imgtotext', async (req, res) => {
       },
     });
 
-    const data = await apiResponse.json();
+    const data = apiResponse.data;
     await unlinkFileAsync(imagePath);
-    //nice
+
     res.json(data);
   } catch (error) {
     res.status(500).json({ error: 'An error occurred' });
