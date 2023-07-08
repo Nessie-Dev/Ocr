556e4d1a4d372b1f839a28d4cb3e66b0a8a793e8
Nessie <63102193+Nessie-Dev@users.noreply.github.com>
2023-07-08 15:30:42 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-import fetch from 'fetch';
+const fetch = require('node-fetch');
 const fs = require('fs');
 const path = require('path');
 const { promisify } = require('util');
